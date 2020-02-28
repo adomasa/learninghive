86876b08d736a65d9a55dbcb6dcca337bca8a8c5
adomas <ramumnesys@gmail.com>
2020-02-28 13:48:22 +0200

    Initial commit

---
+++ src/test/java/distributed/monolith/learninghive/LearninghiveApplicationTests.java
@@ -0,0 +1,13 @@
+package distributed.monolith.learninghive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearninghiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
