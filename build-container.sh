33feb2e4a689fc8bb0141eccfab8da87783cc27b
Domantas.Giedraitis <domantas.giedraitis@vegvisir.lt>
2020-04-05 08:35:48 +0300

    (fix): build script fix

--- build-container.sh
+++ build-container.sh
@@ -1,2 +1,2 @@
-./gradlew build && java -jar build/libs/gs-spring-boot-docker-0.1.0.jar &&
+./gradlew build && java -jar build/libs/*.jar &&
 sudo docker build --build-arg JAR_FILE=build/libs/*.jar -t backend .
