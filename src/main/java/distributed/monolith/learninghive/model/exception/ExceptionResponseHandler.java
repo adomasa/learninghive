200599d1b8b51ae87f35df2002a9474cb863e67e
adomasa <ramumnesys@gmail.com>
2020-05-26 13:49:11 +0300

    Revert lock exception code & minor refactoring
    Signed-off-by: adomasa <ramumnesys@gmail.com>

--- src/main/java/distributed/monolith/learninghive/model/exception/ExceptionResponseHandler.java
+++ src/main/java/distributed/monolith/learninghive/model/exception/ExceptionResponseHandler.java
@@ -1,24 +1,21 @@
 package distributed.monolith.learninghive.model.exception;
 
 import distributed.monolith.learninghive.model.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.OptimisticLockException;
-import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ExceptionResponseHandler extends ResponseEntityExceptionHandler {
 	private static final int RESTRICTION_VIOLATED_STATUS = 432;
-	private static final int LOCK_EXCEPTION_STATUS = 433;
 
 	@ExceptionHandler(RestrictionViolationException.class)
 	public ResponseEntity<ErrorResponse> handleRestrictionViolatedException(RestrictionViolationException ex) {
 		var errorResponse = new ErrorResponse();
-
-		errorResponse.setTimestamp(LocalDateTime.now());
 		errorResponse.setMessage(ex.getMessage());
 		errorResponse.setStatus(RESTRICTION_VIOLATED_STATUS);
 
@@ -28,12 +25,8 @@ public class ExceptionResponseHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(OptimisticLockException.class)
 	public ResponseEntity<ErrorResponse> handleRestrictionViolatedException(OptimisticLockException ex) {
 		var errorResponse = new ErrorResponse();
-
-		errorResponse.setTimestamp(LocalDateTime.now());
 		errorResponse.setMessage("Resource is out of sync with system. Try again.");
-
-		return ResponseEntity.status(LOCK_EXCEPTION_STATUS).body(errorResponse);
+		errorResponse.setStatus(HttpStatus.CONFLICT.value());
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
 	}
-
-
 }
